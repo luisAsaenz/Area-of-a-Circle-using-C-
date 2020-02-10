13ca7c5e6a6e7e36b040ed777c03385ff2922f9d
luisAsaenz <59874758+luisAsaenz@users.noreply.github.com>
2020-02-10 15:24:53 -0500

    Add files via upload
    Area of a circle in C#

---
+++ AreaOfCircle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AreaCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double pI = Math.PI;
+
+            double r;
+            r = Convert.ToDouble(Console.ReadLine());
+
+            if (r > 0)
+            {
+                r = Math.Pow(r , 2);
+                double area = r * pI;
+                Console.WriteLine("The Area is: " + area);
+            }
+
+        }
+    }
+}
